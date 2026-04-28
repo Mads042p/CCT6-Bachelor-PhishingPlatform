ced37406d41544eef1dd83470f7a30e952a81d7b
Tobisius <Tobias.steenberg@outlook.dk>
2026-04-28 16:28:29 +0200

    Deleted printstatements and added comments

--- Website/sqlmanager.py
+++ Website/sqlmanager.py
@@ -1,50 +1,36 @@
 import sqlite3
 
+#Establish the connection and the cursor to the database
 conn = sqlite3.connect("db.db")
 cursor = conn.cursor()
 
+#Harcode the dashboard, since ther's only one.
 tableName = "UserData"
-coloums = ("Name", "Email", "Password", "Company Relation", "IsAdmin")
-email = "JohJohnsen@gmail.com"
 
+#Insert data into the DB, using parameterized quries
 def InsertData(tableName, data):
     query = f"INSERT INTO {tableName} (Name, Email, Password, Company, IsAdmin) VALUES (?, ?, ?, ?, ?);"
     cursor.execute(query, data)
     conn.commit()
 
+#Get the data from the database, based on their email. This returns an array with all of their information.
 def GetData(tableName):
     query = f"SELECT * FROM {tableName}"
     cursor.execute(query)
     return cursor.fetchall()
 
+#Delete a row, based on the users email.
 def DeleteData(tableName, email):
     
     query = f"DELETE FROM {tableName} WHERE Email = ?;"
     cursor.execute(query, (email,))
     conn.commit()
     
-    
+#Get the hashed password, which is used for authentication in the login phase.
 def getHashedPassword(tableName, email):
     
     query = f"SELECT Password FROM {tableName} WHERE Email = ?;"
     cursor.execute(query, (email,))
     conn.commit()
     
-    
     return cursor.fetchall()[0][0]
-
-def main():
-    #InsertData(tableName, data)
-    #DeleteData(tableName, email)
-    print(getHashedPassword(tableName, email))
-    
-
-    """TableData = GetData(tableName)
-    for row in TableData:
-        print("\n")
-        print(row)
-    """
-    
-    
-if __name__ == "__main__":
-    main()
