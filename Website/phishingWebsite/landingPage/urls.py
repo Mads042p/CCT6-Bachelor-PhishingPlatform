f677986e217cc046a7af15d9ec8603217fcdedea
Pjok <82096017+Pjok-o@users.noreply.github.com>
2026-04-29 01:25:15 +0200

    Its Sending mails now.
    Still need to figure out how to pull stuff from database and how mails are structured.

--- Website/phishingWebsite/landingPage/urls.py
+++ Website/phishingWebsite/landingPage/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('newsletter/', views.newsletter, name='newsletter'),
-    
+    path('sendEmail/', views.sendEmail, name='sendEmail'),
 ]
--- Website/phishingWebsite/landingPage/views.py
+++ Website/phishingWebsite/landingPage/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from sqlmanager.views import *
 
-
+import smtplib
+import ssl
+from email.message import EmailMessage
 
 def index(request):
     return render(request, 'landingPage/index.html')
@@ -18,3 +20,26 @@ def newsletter(request):
             insertData('NewsletterEmails', {'email': email})
         return redirect('/')
     return redirect('/')
+
+def sendEmail(request):
+    subject = "Phishing Email from python"
+    body = "This is a phishing email sent from a python script."
+    sender = "phishmedevelopment@gmail.com"
+    to = "rreellaaqq@gmail.com"
+    password = "wbkz tlhu wqzg ekxq"
+
+    em = EmailMessage()
+    em['From'] = sender
+    em['To'] = to
+    em['Subject'] = subject
+    em.set_content(body)
+
+    conttext = ssl.create_default_context()
+    print("Sending email...")
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=conttext) as smtp:
+        smtp.login(sender, password)
+        smtp.sendmail(sender, to, em.as_string())
+    print("Email sent successfully!")
+    return redirect('/')
+
